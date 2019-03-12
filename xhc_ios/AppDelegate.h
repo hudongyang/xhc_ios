1b15c0ee7f848f212d44dbb4547930f515e11e93
hudongyang <whudongyang@163.com>
2019-03-12 08:23:13 +0800

    Initial Commit

---
+++ xhc_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xhc_ios
+//
+//  Created by dongyang on 2019/3/12.
+//  Copyright © 2019 dongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
